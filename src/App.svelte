a00d46385ddc550ffedb37247c458f85f4630e4e
Ruben <rubhvi@kvn.no>
2025-02-11 18:39:19 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<header></header>
+
+<main></main>
+
+<footer></footer>
+
+<style>
+    /* Color */
+    :root{
+        --primary-color: #2c2c2c;
+        --secondary-color: #1f1f1f;
+    }
+
+    /* Styling */
+    header{
+        width: 100%;
+        height: 10%;
+        background-color: var(--secondary-color);
+    }
+    main{
+        width: 100%;
+        height: 90%;
+        background-color: var(--primary-color);
+    }
+</style>
